6cc2384c153ccd6556fcd78b3c578e6468232c0c
koinworks <rickhy.siswanto@koinworks.com>
2019-07-15 00:35:46 +0700

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+
+<script>
+	import Profile from './Profile.svelte';
+
+	const pkg = {
+		name: '' 
+	};
+</script>
+
+<Profile {...pkg}/>
---
+++ src/Profile.svelte
@@ -0,0 +1,19 @@
+<script>
+	export let name;
+
+	let count = 0;
+
+	$: doubled = count + 1;
+	
+	function handleClick() {
+		count += 1;
+		name = 'Rickhy';
+	}
+</script>
+
+<button on:click={handleClick}>
+	Click
+</button>
+
+<p>Hello {name} </p>
+<p>Clicked: {doubled} </p>
